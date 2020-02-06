4a8132a22125c748ccde60bca2b215222aff3699
Chanikya <chanikyaprakash@gmail.com>
2020-02-06 20:40:34 +0530

    completed successfully the inserting login details

--- login/models.py
+++ login/models.py
@@ -1,10 +1,30 @@
 from django.db import models
 
 # Create your models here.
-class userLogin:
+class userLogin(models.Model):
 	"""docstring for userLogin"""
-	def __init__(self,name,email,password):
-		super(userLogin, self).__init__()
-		self.name = name
-		self.email = email
-		self.password = password
+	name=models.CharField(max_length=100,default="")
+	email=models.CharField(max_length=100,default="")
+	password=models.CharField(max_length=100,default="")
+
+	# def __init__(self):
+	# 	self.name = ""
+	# 	self.email = ""
+	# 	self.password = ""
+
+	def __str__(self):
+		return self.name
+
+	def setName(self,name):
+		self.name=name
+
+	def setEmail(self,email):
+		self.email=email
+
+	def setPass(self,password):
+		self.password=password
+
+	def display(self):
+		print("name: ",self.name)
+		print("email: ",self.email)
+		print("passw: ",self.password)
--- login/views.py
+++ login/views.py
@@ -8,6 +8,12 @@ def index(request):
 	if request.method=='POST':
 		# print(json.loads(request.body.decode("utf-8")))
 		data=json.loads(request.body.decode("utf-8"))
-		for i in data:
-			print(data[i])
+		# print(data)
+		usr=userLogin()
+		usr.setName(data['name'])
+		usr.setEmail(data['email'])
+		usr.setPass(data['password'])
+		usr.save()
+	# x=userLogin.objects.all()
+	# print(x)
 	return HttpResponse("Congs! Account Created")
