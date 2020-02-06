c115ca170a71091eee860ed72f54bceb7e460021
Chanikya <chanikyaprakash@gmail.com>
2020-02-06 23:51:23 +0530

    replaced len with count to see no.of entries of same email

--- login/views.py
+++ login/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,JsonResponse
 from .models import userLogin
 import json
 
@@ -9,11 +9,16 @@ def index(request):
 		# print(json.loads(request.body.decode("utf-8")))
 		data=json.loads(request.body.decode("utf-8"))
 		# print(data)
-		usr=userLogin()
-		usr.setName(data['name'])
-		usr.setEmail(data['email'])
-		usr.setPass(data['password'])
-		usr.save()
+		print(userLogin.objects.filter(email=data['email']).count())
+		if 0 == userLogin.objects.filter(email=data['email']).count():
+			usr=userLogin()
+			usr.setName(data['name'])
+			usr.setEmail(data['email'])
+			usr.setPass(data['password'])
+			usr.save()
+			return JsonResponse({"message":"Congs! Account Created","status":True})
+		else:
+			return JsonResponse({"message":"Failed! Account already exist","status":False})
+			# return HttpResponse("Failed! Account already exist")
 	# x=userLogin.objects.all()
 	# print(x)
-	return HttpResponse("Congs! Account Created")
